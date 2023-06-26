d61667b1476433684d93ec141eae2205b1effa07
eMobilis <clarenceyegon@gmail.com>
2023-06-26 10:01:56 +0300

    add bmi file

---
+++ Datatyes.py
@@ -0,0 +1,3 @@
+city = "kisumu" #string
+age = 23 #int
+temp = 37.9812 #float
---
+++ bmi.py
@@ -0,0 +1,9 @@
+weight = input("Enter weight")
+height = input("Enter height")
+
+weight = float(weight)
+height = float(height)
+
+result = weight / (height * height)
+
+print("Your BMI is", result)
