9889f56be7b98c2c458a071c78cbd10cac868fb0
eMobilis <clarenceyegon@gmail.com>
2023-06-27 10:10:57 +0300

    conditionals statement

---
+++ betting.py
@@ -0,0 +1,13 @@
+reward = int(input("Enter your desired number"))
+if reward == 1:
+    print('Golf gti')
+elif reward == 2:
+    print("Free trip to Kisumu")
+elif reward == 3:
+    print("ksh.2000")
+elif reward == 4:
+    print("brand new shoes")
+elif reward == 5:
+    print("syntax error")
+else:
+    print("bad choice")
