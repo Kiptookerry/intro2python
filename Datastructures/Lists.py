4a17cd142c0d75902b87c73184ccdda0473a8e86
eMobilis <clarenceyegon@gmail.com>
2023-06-29 10:32:16 +0300

    tup files

--- Datastructures/Lists.py
+++ Datastructures/Lists.py
@@ -2,12 +2,15 @@
 #you can modify data in a list- add,remove or update
 #a list has order
 
-friends = ["Junior","Glen","Malcolm"]
-print(friends[0])
-friends.append("Roy")
-friends.append("Victor")
-friends.remove("Malcolm")
-print(friends)
+friends = ["Junior","Glen","Malcolm","Victor"]
+
+print(friends[1])
+print(friends[2])
+print(friends[3])
+
+#friends.append("Warama")
+#friends.remove("Malcolm")
+
 
 
 
