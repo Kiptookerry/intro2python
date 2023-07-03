e6eb8ef10621fddc3f1af3033031a005c304b5c9
eMobilis <clarenceyegon@gmail.com>
2023-07-03 09:06:58 +0300

    whileloopex file

---
+++ loops/whileloopex.py
@@ -0,0 +1,11 @@
+#using range 1 - 1000
+#print all numbers using whileloop
+#ensure user inputs number in the terminal
+
+i = int(input("Enter number"))
+while i < 1000:
+    print(i)
+    i += 2
+
+
+
